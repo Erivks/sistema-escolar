7a5089dadf773e5e11d3c8f4d4336d3f0e4413be
Erivks <mordecai.es27@gmail.com>
2019-07-29 14:53:45 -0300

    Model de Matriculas

--- app/Model/Matricula.php
+++ app/Model/Matricula.php
@@ -3,7 +3,27 @@
     class Matricula {
 
         public static getAll(){
-            
+
+            $conn = ConnectionToDB::getConnection();
+
+            $queryRequest = "SELECT id_aluno_curso, a.nome as 'nome_aluno', c.nome as 'nome_curso' 
+                            FROM Alunos_Cursos as ac
+                            INNER JOIN Alunos as a on a.id_aluno = ac.id_aluno
+                            INNER JOIN Cursos as c on c.id_curso = ac.id_curso"
+            $queryRequest = $queryRequest->prepare($queryRequest);
+            $queryRequest-execute();
+
+            $queryResponse = array();
+
+            foreach ($queryRequest->fetchObject('Matricula') as $row) {
+                $queryResponse = $row;
+            }
+
+            if(!$queryResponse){
+                throw new Exception("Não foi possivel encontrar nenhum registro de matriculas.");
+            }
+
+            return $queryResponse
         }
     }
 
