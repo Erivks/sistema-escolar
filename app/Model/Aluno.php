88f34e4e2a7ae268b8e7d3bee12d91aadf19b2f9
Erivks <mordecai.es27@gmail.com>
2019-07-30 11:06:08 -0300

    Implementando função para retornar dados de um único aluno

--- app/Model/Aluno.php
+++ app/Model/Aluno.php
@@ -22,6 +22,24 @@
 
             return $queryResponse;
         }
+
+        public static function getByID($alunoID){
+
+            $conn = ConnectionToDB::getConnection();
+
+            $queryRequest = "SELECT * FROM Alunos :id";
+            $queryRequest = $conn->prepare($queryRequest);
+            $queryRequest->bindValue(':id', $alunoID, PDO::PARAM_INT);
+            $queryRequest->execute();
+
+            $queryResponse = $queryRequest->fetchObject('Aluno');
+
+            if(!$queryResponse){
+                throw new Exception("Não foi possível encontrar nenhum registro de aluno no banco");
+            }
+
+            return $queryResponse;
+        }
     }
 
 ?>
