83bc8368c06034e4f66e62f437b19a239bddf372
Erivks <mordecai.es27@gmail.com>
2019-07-30 11:27:53 -0300

    Mudando a lógica do Core

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -2,9 +2,6 @@
 
     class Core {
         public function start($getURL){
-            
-            $action = 'index';
-
             if(isset($_GET['page'])){
                 $page = ucfirst($_GET['page']).'Controller';
             } else {
@@ -15,7 +12,24 @@
                 $page = 'ErrorController';
             }
 
-            call_user_func_array(array(new $page, $action), array());
+            if(isset($_GET['alter'])){
+
+                $action = 'alterData';
+                $id = $_GET['editar'];
+                call_user_func_array(array(new $page, $action), array('id' => $id));
+            
+            } elseif(isset($_GET['delete'])){
+            
+                $action = 'deleteData';
+                $id = $_GET['deletar'];
+                call_user_func_array(array(new $page, $action), array('id' => $id));
+            
+            } else {
+            
+                $action = 'index';
+                call_user_func_array(array(new $page, $action), array());
+            
+            }
         }
     }
 ?>
