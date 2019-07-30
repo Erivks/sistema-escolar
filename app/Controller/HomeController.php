249704d81642aef2ecc843188788f569f1aa91f5
Erivks <mordecai.es27@gmail.com>
2019-07-30 12:09:03 -0300

    Renderizando template da home

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -3,9 +3,8 @@
     class HomeController {
         public function index(){
             try {
-                $loader = new \Twig\Loader\FilesystemLoader('app/View');
-                $twig = new \Twig\Environment($loader);
-                $template= $twig->load('home.html');
+                $twig = Twig::loadTwig();
+                $template = $twig->load('home.html');
                 echo $template->render();
             } catch (Exception $e) {
                 echo $e;
