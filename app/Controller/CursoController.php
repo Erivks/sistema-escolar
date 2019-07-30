b2f9b007cf2819ad02aacca4455217aea4d53343
Erick <erick_santos00@outlook.com>
2019-07-29 23:19:28 -0300

    Faznedo controller de curso

--- app/Controller/CursoController.php
+++ app/Controller/CursoController.php
@@ -5,7 +5,17 @@
         public function index(){
             try {
                 $storeCursos = Curso::getAll();
-                var_dump($storeCursos);
+
+                $loader = new \Twig\Loader\FilesystemLoader('app/View');
+                $twig = new \Twig\Environment($loader);
+                
+                $template = $twig->load('curso.html');
+                
+                $params = array();
+                $params['cursos'] = $storeCursos;
+
+                $content = $template->render($params);    
+                echo $content;
             } catch (Exception $e) {
                 echo $e->getMessage();
             }
