4cd0911f59c39cc9b12b73e8e91c71fd95383df8
Erivks <mordecai.es27@gmail.com>
2019-07-30 09:49:28 -0300

    Carregando o Twig em uma classe

---
+++ app/lib/Twig/Twig.php
@@ -0,0 +1,13 @@
+<?php
+
+    abstract class Twig {
+
+        public static function loadTwig(){
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader);
+            return $twig;
+        }
+        
+    }
+
+?>
