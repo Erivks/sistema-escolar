9bf5e4364ae8bad5136b677d0932fe2be0b6f2f4
Erivks <mordecai.es27@gmail.com>
2019-08-02 10:00:05 -0300

    Colocando o códgio JS em um arquivo externo

---
+++ app/Template/js/modal.js
@@ -0,0 +1,21 @@
+// This event fires immediately when the show instance method is called. 
+// If caused by a click, the clicked element is available as the relatedTarget 
+// property of the event.
+$('#editarAluno').on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget);
+    var name = button.data('name');
+    var birthday = button.data('birthday');
+
+    var modal = $(this);
+    modal.find('#nameInput').val(name);
+    modal.find('#birthdayInput').val(birthday);
+});
+$('#editarCurso').on('show.bs.modal', function(event){
+    var button = $(event.relatedTarget);
+    var name = button.data('name');
+    var workload = button.data('workload');
+    
+    var modal = $(this);
+    modal.find('#courseNameInput').val(name);
+    modal.find('#courseWorkloadInput').val(workload);
+});
