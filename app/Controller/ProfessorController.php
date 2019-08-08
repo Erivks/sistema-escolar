0e9d08bf385d125c0aea4f1eb3e06ef70aa79f6c
Erivks <mordecai.es27@gmail.com>
2019-08-08 14:01:00 -0300

    Fazendo o professor

---
+++ app/Controller/ProfessorController.php
@@ -0,0 +1,44 @@
+<?php
+    class ProfessorController {
+        public function index() {
+            try {
+                $storeTeacher = Professor::getAll();
+                
+                $twig = Twig::loadTwig();
+                $template = $twig->load('professores.html');
+                
+                $teachers = array();
+                $teachers['teachers'] = $storeTeacher;
+
+                echo $template->render(array('teachers' => $teachers['teachers']));
+            } catch (Exception $error) {
+                echo $error->getMessage();
+            }
+        }
+        public function deleteData($teacherID){
+            try {
+                Professor::deleteTeacher($teacherID);
+                header('location:?page=professor');
+            } catch (Exception $error) {
+                echo $error->getMessage();
+            }
+        }
+        public function alterData(){
+            try {
+                $teacherID = $_POST['idInput'];
+                $teacherName = $_POST['nameInput'];
+                
+                $teacherDataset = array(
+                    'id' => $teacherID,
+                    'name' => $teacherName
+                );
+
+                $queryResponse = Professor::alterTeacher($teacherDataset);
+                header('location:?page=professor');
+            } catch (Exception $error) {
+                echo $error->getMessage();
+            }
+        }
+    }
+
+?>
---
+++ app/Model/Professor.php
@@ -0,0 +1,60 @@
+<?php
+    class Professor {
+        public static function getAll() {
+            
+            $conn = connectionToDB::getConnection();
+
+            $queryRequest = 'SELECT * FROM Professores';
+            $queryRequest = $conn->prepare($queryRequest);
+            $queryRequest->execute();
+            
+            $queryResponse = array();
+            
+            while ($row = $queryRequest->fetchObject('Professor')) {
+                $queryResponse[] = $row;
+            }
+            
+            if(!$queryResponse){
+                throw new Exception("Não foi encontrado nenhum registro de professor");
+            }
+
+            return $queryResponse;
+        }
+        public static function deleteTeacher($teacherID){
+
+            $conn = ConnectionToDB::getConnection();
+
+            $queryRequest = 'DELETE FROM Professor WHERE id_professor = :id';
+            $queryRequest = $conn->prepare($queryRequest);
+            $queryRequest->bindValue(':id', $teacherID, PDO::PARAM_INT);
+            $queryResponse = $queryRequest->execute();
+
+            if(!$queryResponse){
+                throw new Exception("Não foi possível deletar o professor");
+            }
+
+            return $queryResponse;
+        }
+        public static function alterTeacher($teacherDataset){
+
+            $conn = ConnectionToDB::getConnection();
+
+            $teacherID = $teacherDataset['id'];
+            $teacherName = $teacherDataset['name'];
+
+            $queryRequest = 'UPDATE Professor SET nome_professor = :name 
+                            WHERE id_professor = :id';
+            $queryRequest = $conn->prepare($queryRequest);
+            $queryRequest->bindValue(':name', $teacherID, PDO::PARAM_STR);
+            $queryRequest->bindValue(':id', $teacherName, PDO::PARA_INT);
+            $queryResponse = $queryRequest->execute();
+
+            if(!$queryResponse){
+                throw new Exception("Não foi possivel alterar o professor");
+            }
+
+            return $queryResponse;
+        }
+    }
+
+?>
