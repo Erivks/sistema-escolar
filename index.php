9044d3255ab2f31a8eeb754f801cb1fe65c3ee63
Erivks <mordecai.es27@gmail.com>
2019-08-26 15:51:20 -0300

    require nas classes de usuários

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
     require_once 'app/Controller/MatriculaController.php';
     require_once 'app/Controller/ProfessorController.php';
     require_once 'app/Controller/TurmaController.php';
+    require_once 'app/Controller/UserController.php';
     require_once 'app/Model/Curso.php';
     require_once 'app/Model/Matricula.php';
     require_once 'app/Model/Aluno.php';
     require_once 'app/Model/Professor.php';
     require_once 'app/Model/Turma.php';
+    require_once 'app/Model/User.php';
     require_once 'vendor/autoload.php';
 
     $template = file_get_contents('app/Template/estrutura.html');
