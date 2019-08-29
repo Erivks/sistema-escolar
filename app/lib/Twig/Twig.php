7b87706b6cd463777e992ea11cb8751186a55a61
Erivks <mordecai.es27@gmail.com>
2019-08-29 10:16:01 -0300

    adicionando novo metodo ao twig

--- app/lib/Twig/Twig.php
+++ app/lib/Twig/Twig.php
@@ -2,8 +2,15 @@
 
     abstract class Twig {
 
-        public static function loadTwig(){
+        public static function loadTwig()
+        {
             $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader);
+            return $twig;
+        }
+        public static function loadTemplate()
+        {
+            $loader = new \Twig\Loader\FilesystemLoader('app/Template');
             $twig = new \Twig\Environment($loader);
             return $twig;
         }
