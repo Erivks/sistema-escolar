6862a97d8fdd519f3c04796a1f8d51bf24b8e4ef
Erivks <mordecai.es27@gmail.com>
2019-08-29 11:59:40 -0300

    Adicionando método para erro de login

--- app/Controller/ErrorController.php
+++ app/Controller/ErrorController.php
@@ -4,6 +4,12 @@
         public function index() {
             echo 'Erro';
         }
+        public static function errorLogin()
+        {
+            $twig = Twig::loadTwig();
+            $template = $twig->load('errorLogin.html');
+            echo $template->render(array());
+        }
     }
 
 ?>
