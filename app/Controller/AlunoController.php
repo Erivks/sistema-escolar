205b87fa6f6fff40b244ab0867d28134f5481ac2
Erivks <mordecai.es27@gmail.com>
2019-08-30 16:17:32 -0300

    Usando o método criado em UserController para verificação de login

--- app/Controller/AlunoController.php
+++ app/Controller/AlunoController.php
@@ -1,7 +1,8 @@
 <?php
 
     class AlunoController {
-        public function index(){
+        public function index()
+        {
             try {
                 //Retornando dados de todos os alunos
                 $storeAlunos = Aluno::getAll();
@@ -11,82 +12,74 @@
                 $template = $twig->load('alunos.html');
 
                 //Criando um array para melhor identificação dos campos
-                $alunos = array();
-                $alunos['alunos'] = $storeAlunos;
+                $alunos = array(
+                    'alunos' => $storeAlunos
+                );
 
                 //Renderizando template
-                if(isset($_SESSION['userId']))
-                {   
+                if (UserController::verifiyLogin()) {   
                     echo $template->render(array(
                         'alunos' => $alunos['alunos'],
-                        'session' => $_SESSION
                     ));
-                } else 
-                {
-                    ErrorController::errorLogin();
                 }
-            
             } catch (Exception $error) {
-                $twig = Twig::loadTwig();
-                $template = $twig->load('alunos.html');
-
-                $message = $error->getMessage();
-                if(isset($_SESSION['userId']))
-                {
+                if (UserController::verifiyLogin()) {
+                    $twig = Twig::loadTwig();
+                    $template = $twig->load('alunos.html');
+                    $message = $error->getMessage();
                     echo $template->render(array(
                         'message' => $message,
                     ));
-                } else 
-                {
-                    ErrorController::errorLogin();
                 }
             }
         }
-        public function deleteData($alunoID){
+        public function deleteData($alunoID)
+        {
             try {
-                Aluno::deleteByID($alunoID);
-                header('location:?page=aluno');
+                if (UserController::verifyLogin()) {
+                    Aluno::deleteByID($alunoID);
+                    header('location:?page=aluno');
+                }
             } catch(Exception $error){
                 echo $error->getMessage();
             }
         }
-        public function alterData(){
+        public function alterData()
+        {
             try {
-                
-                $studentID = $_POST['idInput'];
-                $studentName = $_POST['nameInput'];
-                $studentBirthday = $_POST['birthdayInput'];
-
-                $studentDataset = array(
-                    'id' => $studentID, 
-                    'name' => $studentName, 
-                    'birthday' => $studentBirthday
-                );
-
-                $queryResponse = Aluno::alterData($studentDataset);
-                header('location:?page=aluno');
+                if (UserController::verifyLogin()) {
+                    $studentID = $_POST['idInput'];
+                    $studentName = $_POST['nameInput'];
+                    $studentBirthday = $_POST['birthdayInput'];
+                    $studentDataset = array(
+                        'id' => $studentID, 
+                        'name' => $studentName, 
+                        'birthday' => $studentBirthday
+                    );
+                    Aluno::alterData($studentDataset);
+                    header('location:?page=aluno');
+                }
             } catch(Exception $error){
                 echo $error->getMessage();
             }
         }
-        public function insertData(){
-            try {
-                
-                $studentName = $_POST['nameInput'];
-                $studentBirthday = $_POST['birthdayInput'];
-                $studentBirthday = DateTime::createFromFormat('d/m/Y', $studentBirthday);
-                $studentBirthday = $studentBirthday->format('Y-m-d');
-                $studentDataset = array(
-                    'name' => $studentName,
-                    'birthday' => $studentBirthday
-                );
-
-                $queryResponse = Aluno::insertData($studentDataset);
-                header('location:?page=aluno');
+        public function insertData()
+        {
+            try {                
+                if (UserController::verifyLogin()) {
+                    $studentName = $_POST['nameInput'];
+                    $studentBirthday = $_POST['birthdayInput'];
+                    $studentBirthday = DateTime::createFromFormat('d/m/Y', $studentBirthday);
+                    $studentBirthday = $studentBirthday->format('Y-m-d');
+                    $studentDataset = array(
+                        'name' => $studentName,
+                        'birthday' => $studentBirthday
+                    );
+                    Aluno::insertData($studentDataset);
+                    header('location:?page=aluno');
+                }
             } catch (Exception $error) {
                 echo $error->getMessage();
             }
         }
     }
-
-?>
--- app/Controller/CursoController.php
+++ app/Controller/CursoController.php
@@ -1,87 +1,83 @@
 <?php
 
-    class CursoController {
-
-        public function index(){
+    class CursoController 
+    {
+        public function index()
+        {
             try {
                 $storeCursos = Curso::getAll();
 
                 $twig = Twig::loadTwig();
                 $template = $twig->load('cursos.html');
                 
-                $cursos = array();
-                $cursos['cursos'] = $storeCursos;
+                $cursos = array(
+                    'cursos' => $storeCursos
+                );
 
-                if(isset($_SESSION['userId']))
-                {
+                if (UserController::verifyLogin()) {
                     echo $template->render(array(
                         'cursos' => $cursos['cursos'],
-                    )); 
-                } else 
-                {
-                   ErrorController::errorLogin();
+                    ));
                 }
-                
             } catch (Exception $error) {
-                $twig = Twig::loadTwig();
-                $template = $twig->load('cursos.html');
-
-                $message = $error->getMessage();
-                if(isset($_SESSION['userId']))
-                {
+                if (UserController::verifyLogin()) {
+                    $twig = Twig::loadTwig();
+                    $template = $twig->load('cursos.html');
+                    $message = $error->getMessage();
                     echo $template->render(array(
                         'message' => $message
                     ));
-                } else
-                {
-                    ErrorController::errorLogin();
                 }
             }
         }
 
-        public function deleteData($cursoID){
+        public function deleteData($cursoID)
+        {
             try {
-                Curso::deleteByID($cursoID);
-                header('location:?page=curso');
+                if (UserController::verifyLogin()) {
+                    Curso::deleteByID($cursoID);
+                    header('location:?page=curso');
+                }
             } catch (Exception $error) {
                 echo $error->getMessage();
             }
         }
 
-        public function alterData(){
+        public function alterData()
+        {
             try {
-                $courseID = $_POST['idInput'];
-                $courseName = $_POST['nameInput'];
-                $courseWorkload = $_POST['workloadInput'];
-
-                $courseDataset = array(
-                    'id' => $courseID,
-                    'name' => $courseName,
-                    'workload' => $courseWorkload
-                );
-                $queryResponse = Curso::alterData($courseDataset);
-                header('location:?page=curso');
+                if (UserController::verifyLogin()) {
+                    $courseID = $_POST['idInput'];
+                    $courseName = $_POST['nameInput'];
+                    $courseWorkload = $_POST['workloadInput'];
+                    $courseDataset = array(
+                        'id' => $courseID,
+                        'name' => $courseName,
+                        'workload' => $courseWorkload
+                    );
+                    Curso::alterData($courseDataset);
+                    header('location:?page=curso');
+                }
             } catch (Exception $error) {
                 echo $error->getMessage();
             }
         }
 
-        public function insertData(){
+        public function insertData()
+        {
             try {
-                $courseName = $_POST['nameInput'];
-                $courseWorkload = $_POST['workloadInput'];
-
-                $courseDataset = array(
-                    'name' => $courseName,
-                    'workload' => $courseWorkload
-                );
-
-                $queryResponse = Curso::insertData($courseDataset);
-                header('location:?page=curso');
+                if (UserController::verifyLogin()) {
+                    $courseName = $_POST['nameInput'];
+                    $courseWorkload = $_POST['workloadInput'];
+                    $courseDataset = array(
+                        'name' => $courseName,
+                        'workload' => $courseWorkload
+                    );
+                    $queryResponse = Curso::insertData($courseDataset);
+                    header('location:?page=curso');
+                }
             } catch (Exception $error) {
                 echo $error->getMessage();
             }
         }
     }
-
-?>
